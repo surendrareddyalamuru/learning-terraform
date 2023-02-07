177841f3a6b1e84aa91868b9c230cd2a893f4526
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-07 10:37:33 +0530

    var.tf

--- 02-variables/var.tf
+++ 02-variables/var.tf
@@ -9,3 +9,27 @@ output "sample" {
 output "sample1" {
   value = "Hey, ${var.sample}"
 }
+
+variable "sample1" {
+  default = "Hello"
+}
+
+variable "sample2" {
+  default = 100
+}
+
+variable "sample3" {
+  default = True
+}
+
+variable "sample4" {
+  default = [
+    "Hello"
+    100
+    True
+  ]
+}
+
+output "sample4" {
+  value = var.sample4
+}
