a2b4a0cc3c64dd6b6e585965fda7c8a07981234c
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-08 09:52:33 +0530

    functions

--- 05-functions/main.tf
+++ 05-functions/main.tf
@@ -5,3 +5,21 @@ variable "sample" {
 output "sample" {
   value = element(var.sample, 0)
 }
+
+varible "sample1" {
+  default = {
+    orange = {
+      Name = orange
+      count = 100
+    }
+    apple = {
+      Name = apple
+      count = 100
+    }
+  }
+}
+
+output "sample1" {
+  value = lookup(var.sample1, apple, null)
+
+}
