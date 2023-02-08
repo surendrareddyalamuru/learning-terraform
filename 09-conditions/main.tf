331cb474c444feacb9a4c7fb894a9a8db4ecb98b
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-08 14:00:38 +0530

    conditions

--- 09-conditions/main.tf
+++ 09-conditions/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh.*.id]
+  vpc_security_group_ids = aws_security_group.allow_ssh.*.id
 
   tags = {
     Name = "test"
