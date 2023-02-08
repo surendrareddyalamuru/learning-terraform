fa75c2de168b7c70d1ebbf798cfd52803f5409c0
surendra reddy <surendrareddylamuru01@gmail.com>
2023-02-08 09:31:28 +0530

    resources

--- 03-resources/04-resource-state/ec2.tf
+++ 03-resources/04-resource-state/ec2.tf
@@ -2,4 +2,10 @@ resource "aws_instance" "sample" {
   ami = "ami-0a017d8ceb274537d"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = upper(var.tagname)
+  }
 }
+
+variable "tagname" {}
